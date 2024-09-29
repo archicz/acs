0c6c394d1019cef0da7ea8834a86e8c30ed65e28
archi <52219324+archicz@users.noreply.github.com>
2024-09-29 12:45:38 +0200

    This script now follows the same code-style as the other scripts

--- lua/acs/libs/sh_analogmapper.lua
+++ lua/acs/libs/sh_analogmapper.lua
@@ -2,29 +2,29 @@ local PercentMapper = {}
 PercentMapper.__index = PercentMapper
 
 function PercentMapper:New(strength)
-    self.strength = strength or 0.1
-    self.nextProgress = 0
-    self.inputValue = 0
-    self.outputValue = 0
+    self.Strength = strength or 0.1
+    self.NextProgress = 0
+    self.InputValue = 0
+    self.OutputValue = 0
 end
 
 function PercentMapper:Input(inValue)
     if inValue ~= nil then
-        self.inputValue = math.Clamp(inValue, -1, 1)
+        self.InputValue = math.Clamp(inValue, -1, 1)
     end
 end
 
 function PercentMapper:Output()
-    local delta = self.inputValue - self.outputValue
+    local delta = self.InputValue - self.OutputValue
     local deltaDir = (delta > 0) and 1 or -1
-    local strength = FrameTime() * self.strength
+    local strength = FrameTime() * self.Strength
     local remaining = math.abs(delta) < strength
     
-    local outputValue = remaining and self.inputValue or (self.outputValue + (strength * deltaDir))
+    local outputValue = remaining and self.InputValue or (self.OutputValue + (strength * deltaDir))
     local clamped = math.Clamp(outputValue, -1, 1)
     local rounded = math.Round(clamped, 2)
 
-    self.outputValue = clamped
+    self.OutputValue = clamped
     return rounded
 end
 
