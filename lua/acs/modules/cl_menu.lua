6571d8b3139a48192d90710b8ce1ea5790d3ab36
archi <52219324+archicz@users.noreply.github.com>
2024-09-29 23:41:37 +0200

    This is now mostly the interactivescene testbench

--- lua/acs/modules/cl_menu.lua
+++ lua/acs/modules/cl_menu.lua
@@ -12,10 +12,9 @@ local hangarCamera = {}
 
 local function PrototypeScene()
     hangarScene = interactivescene.CreateScene()
-    hangarScene:CreateCamera(Vector(175, -240, 200), Angle(25, 125, 0), 90)
+    hangarScene:CreateCamera(Vector(175, -240, 200), Angle(25, 125, 0), 100)
     hangarScene:SetSkybox("skybox/sky_day02_02")
-    -- hangarScene:CreateCamera(Vector(0, 0, 0), Angle(0, 0, 0), 100)
-
+    
     local pacData = pace.luadata.ReadFile("pac3/hangar_alpha.txt")
 
     for _, entry in pairs(pacData[1]["children"]) do
@@ -25,7 +24,6 @@ local function PrototypeScene()
         prop:SetPos(partData["Position"])
         prop:SetAngles(partData["Angles"])
         prop:SetScale(partData["Scale"])
-        -- prop:SetDirectionalLight(BOX_TOP, Color(100, 100, 100, 255))
         prop:SetModel(partData["Model"])
 
         hangarScene:AddObject(prop)
@@ -76,57 +74,6 @@ hook.Add("DrawOverlay", "NegrDraw", function()
         imgui.BeginWindow("Settings", IMGUI_POS_CENTER, IMGUI_POS_CENTER, 1280, 720)
             imgui.SetPadding(2, 2, 2, 2)
             imgui.SameLine()
-
-            imgui.BeginGroup(300, IMGUI_SIZE_CONTENT)
-                imgui.SetPadding(0, 0, 0, 2)
-
-                imgui.BeginScrollGroup(IMGUI_SIZE_CONTENT, 400, scroll)
-                    imgui.SetPadding(2, 2, 2, 2)
-
-                    -- hangarCamera.FOV = imgui.SliderDecimal("Cam FOV", 0, 140, hangarCamera.FOV)
-
-                    local sceneObjects = hangarScene:GetObjects()
-
-                    for i = 1, #sceneObjects do
-                        local obj = sceneObjects[i]
-                        if obj.Class != INTERACTIVESCENE_CLASS_PROP then continue end
-                        
-                        if imgui.Button("#" .. i .. " " .. obj:GetModel(), IMGUI_SIZE_CONTENT, 25) then
-                            selectedObj = i
-
-                            cringeX = 0
-                            cringeY = 0
-                            cringeZ = 0
-                        end
-                    end
-                scroll = imgui.EndScrollGroup()
-
-                imgui.SetPadding(0, 0, 0, 0)
-
-                imgui.BeginScrollGroup(IMGUI_SIZE_CONTENT, IMGUI_SIZE_CONTENT, scroll2)
-                    imgui.SetPadding(2, 2, 2, 2)
-
-                    if selectedObj > 0 then
-                        local sceneObjects = hangarScene:GetObjects()
-                        local obj = sceneObjects[selectedObj]
-                        local mdl = obj:GetModel()
-                        local pos = obj:GetPos()
-                        local ang = obj:GetAngles()
-                        
-                        imgui.Label(mdl)
-                        imgui.Label(string.format("Pos: %2f, %2f, %2f", pos.x, pos.y, pos.z))
-                        imgui.Label(string.format("Ang: %2f, %2f, %2f", ang.p, ang.y, ang.r))
-
-                        cringeX = imgui.SliderDecimal("Pos X Offset", -100, 100, cringeX)
-                        cringeY = imgui.SliderDecimal("Pos Y Offset", -100, 100, cringeY)
-                        cringeZ = imgui.SliderDecimal("Pos Z Offset", -100, 100, cringeZ)
-
-                        obj.PosOffset.x = cringeX
-                        obj.PosOffset.y = cringeY
-                        obj.PosOffset.z = cringeZ
-                    end
-                scroll2 = imgui.EndScrollGroup()
-            imgui.EndGroup(true)
 
             imgui.BeginGroup(IMGUI_SIZE_CONTENT, IMGUI_SIZE_CONTENT)
                 imgui.SetPadding(2, 2, 2, 2)
