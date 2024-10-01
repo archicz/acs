03c8b6836cf523c07b81aab5a959b04dc6b413fa
archi <52219324+archicz@users.noreply.github.com>
2024-10-01 12:53:46 +0200

    Added 3D imgui to interactivescene

--- lua/acs/libs/cl_interactivescene.lua
+++ lua/acs/libs/cl_interactivescene.lua
@@ -196,6 +196,62 @@ end
 
 
 
+local SceneObjectUI = {}
+SceneObjectUI.__index = SceneObjectUI
+
+function SceneObjectUI:New()
+    self.Pos = Vector(0, 0, 0)
+    self.Ang = Angle(0, 0, 0)
+    self.Scale = 0.1
+    self.Context = {}
+end
+
+function SceneObjectUI:DoGUI()
+end
+
+function SceneObjectUI:PreDraw(camera)
+end
+
+function SceneObjectUI:Draw(camera)
+    local pos = self.Pos
+    local ang = self.Ang
+    local scale = self.Scale
+    local ctx = self.Context
+
+    local camPos = camera.Pos
+    local camAng = camera.Ang + camera.ViewAngles // USES DEBUG CODE
+    local camFOV = camera.FOV
+    local camX = camera.ScreenX
+    local camY = camera.ScreenY
+    local camW = camera.ScreenW
+    local camH = camera.ScreenH
+
+    local cursorX, cursorY = input.GetCursorPos()
+    local sceneDir = util.AimVector(camAng, camFOV, cursorX - camX, cursorY - camY, camW, camH)
+    sceneDir:Normalize()
+
+    local planeIntersect = util.IntersectRayWithPlane(camPos, sceneDir, pos, ang:Up())
+    local planeX = 0
+    local planeY = 0
+
+    if planeIntersect then
+        local diff = (pos - planeIntersect)
+        planeX = diff:Dot(-ang:Forward()) / scale
+        planeY = diff:Dot(-ang:Right()) / scale
+    end
+
+    cam.Start3D2D(pos, ang, scale)
+    imgui.Context3D2D(ctx, planeX, planeY, input.IsMouseDown(MOUSE_LEFT))
+        self:DoGUI()
+    imgui.ContextEnd()
+    cam.End3D2D()
+end
+
+function SceneObjectUI:PostDraw(camera)
+end
+
+
+
 local SceneCamera = {}
 SceneCamera.__index = SceneCamera
 
@@ -542,6 +598,14 @@ function interactivescene.CreateSprite()
     return instance
 end
 
+function interactivescene.CreateUI()
+    local instance = {}
+    setmetatable(instance, SceneObjectUI)
+    instance:New()
+
+    return instance
+end
+
 function interactivescene.CreateScene()
     local instance = {}
     setmetatable(instance, Scene)
@@ -556,7 +620,6 @@ function interactivescene.DrawRT(scene, rt)
     local w = rt:Width()
     local h = rt:Height()
 
-    // Death sentence to the people responsible for documenting transparency and rendertargets.
     render.PushRenderTarget(rt)
     render.OverrideAlphaWriteEnable(true, true)
         render.ClearDepth()
@@ -591,3 +654,150 @@ function imgui.SceneViewer(scene, w, h)
 
     imgui.ContentAdd(w, h)
 end
+
+/*
+local nigger = false
+local prevNigger = false
+local tglNigger = false
+
+local ctx = {}
+local ctx2 = {}
+
+local Props = {}
+local FarZ = 4096
+
+local CamPos = Vector(45, 0, 70)
+local LookAng = Angle(0, 180, 0)
+local FOV = 100
+local AmbientLight = Color(100, 100, 100)
+local Kolour = Color(255, 255, 255)
+
+function ZapniKreteni()
+    for _, entry in pairs(kokoti[1]["children"]) do
+        local partData = entry["self"]
+        
+        local prop = ClientsideModel(partData["Model"])
+        if IsValid(prop) then
+            prop:SetNoDraw(true)
+            prop:SetIK(false)
+            prop:SetPos(partData["Position"])
+            prop:SetAngles(partData["Angles"])
+    
+            table.insert(Props, prop)
+        end
+    end
+end
+
+function Kreteni(x, y, w, h)
+    surface.SetDrawColor(50, 50, 50)
+    surface.DrawRect(x, y, w, h)
+
+    local up = input.IsKeyDown(KEY_PAD_8) and -1 or 0
+    local down = input.IsKeyDown(KEY_PAD_2) and 1 or 0
+
+    local left = input.IsKeyDown(KEY_PAD_4) and 1 or 0
+    local right = input.IsKeyDown(KEY_PAD_6) and -1 or 0
+
+    LookAng.p = LookAng.p + (up + down)
+    LookAng.y = LookAng.y + (left + right)
+
+	cam.Start3D(CamPos, LookAng, FOV, x, y, w, h, 5, FarZ)
+
+	render.SuppressEngineLighting( true )
+	render.SetLightingOrigin( Vector(0, 0, 0) )
+	render.ResetModelLighting( AmbientLight.r / 255, AmbientLight.g / 255, AmbientLight.b / 255 )
+	render.SetColorModulation( Kolour.r / 255, Kolour.g / 255, Kolour.b / 255 )
+	render.SetBlend( 1 ) -- * surface.GetAlphaMultiplier()
+
+	//for i = 0, 6 do
+	//	local col = self.DirectionalLight[ i ]
+	//	if ( col ) then
+	//		render.SetModelLighting( i, col.r / 255, col.g / 255, col.b / 255 )
+	//	end
+	//end
+
+    render.ClearDepth( false )
+    for _, prop in pairs(Props) do
+        prop:DrawModel()
+    end
+
+    local pozicka = Vector(20, 0, 75)
+    local angl = Angle(0, 90, 90)
+    local skal = 0.05
+
+    local kurzoresX, kurzoresY = input.GetCursorPos()
+    local smericek = util.AimVector(LookAng, FOV, kurzoresX - x, kurzoresY - y, w, h)
+    smericek:Normalize()
+
+    local testHovna = util.IntersectRayWithPlane(CamPos, smericek, pozicka, angl:Up())
+
+    local rozdl = pozicka - testHovna
+    local skutecneX = rozdl:Dot(-angl:Forward()) / skal
+    local skutecneY = rozdl:Dot(-angl:Right()) / skal
+
+    local sracka = Props[23]
+    local obbMin, obbMax = sracka:GetModelBounds()
+    local srackAng = sracka:GetAngles()
+    local srackPos = sracka:GetPos() + sracka:OBBCenter()
+
+    render.DrawWireframeBox(srackPos, srackAng, obbMin, obbMax, Color(255, 0, 0))
+
+    local hitPos, hitNormal, frac = util.IntersectRayWithOBB(CamPos, smericek * 1000, srackPos, srackAng, obbMin, obbMax)
+    if hitPos then
+        render.DrawLine(Vector(0, 0, 0), hitPos, Color(255, 0, 0), false)
+    end
+    
+    cam.Start3D2D(pozicka, angl, skal)
+        imgui.Context3D2D(ctx2)
+        ctx2.MouseX = skutecneX
+        ctx2.MouseY = skutecneY
+            imgui.BeginWindow("Settings", 0, 0, IMGUI_SIZE_CONTENT, IMGUI_SIZE_CONTENT)
+                imgui.SetPadding(2, 2, 2, 2)
+                
+                imgui.BeginGroup(IMGUI_SIZE_CONTENT, 44)
+                    imgui.SetPadding(2, 2, 2, 2)
+                    imgui.Button("3D2D ImGUI Test", IMGUI_SIZE_CONTENT, 40)
+                imgui.EndGroup()
+            imgui.EndWindow()
+        imgui.ContextEnd()
+
+        --surface.SetDrawColor(200, 0, 0)
+        --surface.DrawRect(skutecneX, skutecneY, 2, 2)
+    cam.End3D2D()
+
+	render.SuppressEngineLighting( false )
+	cam.End3D()
+end
+
+ZapniKreteni()
+
+hook.Add("DrawOverlay", "NegrDraw", function()
+    prevNigger = nigger
+    nigger = input.IsKeyDown(KEY_F3)
+
+    if nigger and not prevNigger then
+        tglNigger = !tglNigger
+    end
+
+    if not tglNigger then return end
+
+    input.UnlockCursor()
+    imgui.Context2D(ctx)
+        imgui.BeginWindow("Settings", IMGUI_POS_CENTER, IMGUI_POS_CENTER, 800, 600)
+            imgui.SetPadding(2, 2, 2, 2)
+            
+            imgui.BeginGroup(IMGUI_SIZE_CONTENT, 44)
+                imgui.SetPadding(2, 2, 2, 2)
+                imgui.Button("test1", IMGUI_SIZE_CONTENT, 40)
+            imgui.EndGroup()
+
+            imgui.BeginGroup(IMGUI_SIZE_CONTENT, IMGUI_SIZE_CONTENT)
+                imgui.SetPadding(2, 2, 2, 2)
+                imgui.Custom(IMGUI_SIZE_CONTENT, IMGUI_SIZE_CONTENT, Kreteni)
+            imgui.EndGroup()
+
+        imgui.EndWindow()
+    imgui.ContextEnd()
+end)
+
+*/
