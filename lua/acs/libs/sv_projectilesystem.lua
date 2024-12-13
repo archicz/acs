ced261db0e29c7fadfdb9b3a7297d28f1ae2c595
archi <52219324+archicz@users.noreply.github.com>
2024-12-13 11:34:07 +0100

    fixed spread, math.Rand produces random floats instead of math.random

--- lua/acs/libs/sv_projectilesystem.lua
+++ lua/acs/libs/sv_projectilesystem.lua
@@ -5,19 +5,15 @@ local ActiveProjectiles = projectilesystem.GetActive()
 local Projectile = projectilesystem.GetProjectileMeta()
 
 function projectilesystem.MakeVelocity(dir, velScalar, spreadAmount)
-    local uniqueNumber = SysTime() + engine.TickInterval()
-    local randomSeed = util.CRC(tostring(uniqueNumber))
-    math.randomseed(randomSeed)
-
-    local spreadAng = Angle(
-        math.random(-spreadAmount.p, spreadAmount.p),
-        math.random(-spreadAmount.y, spreadAmount.y),
-        math.random(-spreadAmount.r, spreadAmount.r)
+   local spreadAng = Angle(
+        math.Rand(-spreadAmount.p, spreadAmount.p),
+        math.Rand(-spreadAmount.y, spreadAmount.y),
+        math.Rand(-spreadAmount.r, spreadAmount.r)
     )
 
     local dirAng = dir:Angle()
     local finalDir = (dirAng + spreadAng):Forward()
     local velocity = finalDir * velScalar
-    
+
     return velocity
 end
